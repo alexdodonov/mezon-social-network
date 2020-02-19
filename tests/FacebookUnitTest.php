f4787381be7fcaef4fa655fad262dccb78ef2292
Dodonov <alexey@dodonov.pro>
2020-02-19 17:41:22 +0300

    bugfix/Unit-tests were improved

--- tests/FacebookUnitTest.php
+++ tests/FacebookUnitTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once (__DIR__ . '/../vendor/autoload.php');
 
 class FacebookUnitTest extends PHPUnit\Framework\TestCase
 {
--- tests/VKAuthUnitTest.php
+++ tests/VKAuthUnitTest.php
@@ -1,5 +1,4 @@
 <?php
-require_once (__DIR__ . '/../vendor/autoload.php');
 
 class VKAuthUnitTest extends PHPUnit\Framework\TestCase
 {
